a11c48bae6edc6ea29e95e05897c7385d5fad44d
celina1220 <wangweixidian@gmail.com>
2021-03-06 09:48:32 -0800

    Added Product Updated

---
+++ src/main/java/com/laioffer/onlineShop/Application.java
@@ -0,0 +1,21 @@
+package com.laioffer.onlineShop;
+import com.laioffer.onlineShop.log.PaymentAction;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.math.BigDecimal;
+
+public class Application {
+    public static void main(String[] args) {
+
+//        Logger logger = new ServerLogger();
+//        PaymentAction paymentAction = new PaymentAction(logger);
+//        paymentAction.pay(new BigDecimal(2));
+
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "payment.xml");
+
+        PaymentAction paymentAction = (PaymentAction) context.getBean(PaymentAction.class);
+        paymentAction.pay(new BigDecimal(2));
+    }
+}
--- src/main/java/onlineShop/log/ServerLogger.java
+++
@@ -1,7 +0,0 @@
-package onlineShop.log;
-
-public class ServerLogger implements Logger {
-    public void log(String info) {
-        System.out.println("server log = " + info);
-    }
-}
